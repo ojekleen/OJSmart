5fc336e9f426bd4f2e8fc07c653f3aba399a558f
ojekleen <ojek@163.com>
2015-04-07 15:07:55 +0800

    1

--- HelloWord/WinApp/ControlForm.Designer.cs
+++ HelloWord/WinApp/ControlForm.Designer.cs
@@ -28,19 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblShow = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(53, 37);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(119, 46);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "开始";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblShow
+            // 
+            this.lblShow.AutoSize = true;
+            this.lblShow.Location = new System.Drawing.Point(209, 70);
+            this.lblShow.Name = "lblShow";
+            this.lblShow.Size = new System.Drawing.Size(65, 12);
+            this.lblShow.TabIndex = 1;
+            this.lblShow.Text = "处理条数：";
             // 
             // ControlForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(295, 279);
+            this.ClientSize = new System.Drawing.Size(630, 458);
+            this.Controls.Add(this.lblShow);
+            this.Controls.Add(this.btnStart);
             this.Name = "ControlForm";
             this.Text = "ControlForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblShow;
     }
 }
--- HelloWord/WinApp/ControlForm.cs
+++ HelloWord/WinApp/ControlForm.cs
@@ -16,5 +16,33 @@ namespace WinApp
         {
             InitializeComponent();
         }
+
+        private delegate void InvokeDelegate(int i);
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            System.Threading.Thread thread = new System.Threading.Thread(Start);
+            thread.Start();
+        }
+
+        private void Start()
+        {
+            if (this.lblShow.InvokeRequired)
+            {
+                InvokeDelegate invDelegate = new InvokeDelegate(SetText);
+
+                for (int i = 0; i < 100; i++)
+                {
+                    this.Invoke(invDelegate, new object[] { i });
+                    TimeSpan ts = new TimeSpan(0, 0, 1);
+                    System.Threading.Thread.Sleep(ts);
+                }
+            }
+        }
+
+        private void SetText(int i)
+        {
+            this.lblShow.Text = string.Format("处理条数：{0}", i);
+        }
     }
 }
